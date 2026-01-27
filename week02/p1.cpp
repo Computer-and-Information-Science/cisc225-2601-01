62df6c281c77dbad99802c3b1482bf89fe715fc0
nlippincott <nl@acm.org>
2026-01-27 12:44:38 -0500

    Added week 2 resources

---
+++ week02/p1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+// #include <iomanip>
+using namespace std;
+
+int main () {
+
+	unsigned char x, y, z;
+	x = 200;
+	y = 100;
+	z = x + y;
+
+	cout << "x=" << +x << " y=" << +y << " z=" << +z << endl;
+	// cout << hex;
+	// cout << "x=" << +x << " y=" << +y << " z=" << +z << endl;
+}
